12948e72d33151933188ce78dd399fa8a7e8ce85
RitaDyvnych <m.dyvnych@gmail.com>
2021-11-20 11:45:53 +0200

    base install

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import style from "./components/field.module.css";
+
+
+export default function App() {
+
+
+  return (
+      <div className="App">
+        <h1 className={style.title}>Tic Tac Toe</h1>
+      </div>
+    );
+}
